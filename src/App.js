00ace323f6a99e1cb08fba9403a1a8f75718c78e
RJDM02 <rauljavierdominguezmaymir@gmail.com>
2025-05-22 22:26:45 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route, Navigate, useLocation, Outlet } from "react-router-dom";
 import RequireAuth from "./components/RequireAuth"; 
+import InactivityDetector from "./components/InactivityDetector"; // Importa el nuevo componente
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -10,6 +11,7 @@ import Home from "./components/global/home";
 import Changerpassword from "./components/global/changerpassword";
 import Navbar from "./components/global/navbar";
 import Unauthorized from "./components/global/unauthorized";
+import MontosExtra from "./components/global/monto_extra"
 
 // gestRol
 import ListaUsuarios from "./components/gestRol/listaUsuarios";
@@ -64,79 +66,94 @@ const Layout = () => {
 };
 
 function App() {
+  const handleLogout = () => {
+    // Lógica para cerrar sesión
+    localStorage.removeItem("auth");
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    window.location.href = "/"; // Redirige al login
+  };
+
   return (
     <Router>
-      <Routes>
+      {/* Envuelve todo con el InactivityDetector */}
+      <InactivityDetector 
+        onLogout={handleLogout}
+        inactivityTime={30} // 30 minutos de inactividad
+        warningTime={5} // Muestra advertencia 5 minutos antes
+      >
+        <Routes>
+          <Route path="/" element={<Login />} />
 
-        <Route path="/" element={<Login />} />
+          {/* Layout general con navbar */}
+          <Route element={<Layout />}>
+            {/* Rutas protegidas para todos los roles */}
+            <Route element={<RequireAuth allowedRoles={["SuperAdmin", "Admin", "user_User", "user_Supervisor", "user_Supervisado"]} />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
 
-        {/* Layout general con navbar */}
-        <Route element={<Layout />}>
+            <Route element={<RequireAuth allowedRoles={["SuperAdmin", "Admin", "user_User", "user_Supervisor", "user_Supervisado"]} />}>
+              <Route path="/changerPassword" element={<Changerpassword />} />
+            </Route>
 
-          {/* Rutas protegidas para todos los roles */}
-          <Route element={<RequireAuth allowedRoles={["SuperAdmin", "Admin", "user_User", "user_Supervisor", "user_Supervisado"]} />}>
-            <Route path="/home" element={<Home />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={["SuperAdmin", "Admin", "user_User", "user_Supervisor", "user_Supervisado"]} />}>
-            <Route path="/changerPassword" element={<Changerpassword />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={["Admin", "user_User", "user_Supervisor", "user_Supervisado"]} />}>
-            <Route path="/gestCobro/listarCobros" element={<ListarCobros />} />
-            <Route path="/gestCobro/insertarCobro" element={<InsertarCobro />} />
-            <Route path="/gestCobro/modificarCobro/:id" element={<ModificarCobro />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={["SuperAdmin"]} />}>
-            {/* Rol */}
-            <Route path="/gestRol/listaUsuarios" element={<ListaUsuarios />} />
-            <Route path="/modificarRol/:id" element={<ModificarRol />} />
-            {/* PagoCliente */}
-            <Route path="/superVisarPagoCliente/listarPagosClientes" element={<ListarPagosClientes />} />
-            <Route path="/superVisarPagoCliente/modificarPagoCliente/" element={<ModificarPagoCliente />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={["SuperAdmin", "Admin"]} />}>
-            {/* Trabajador */}
-            <Route path="/gestTrabajador/listaTrabajadores" element={<ListaTrabajadores />} />
-            <Route path="/gestTrabajador/insertarTrabajador" element={<InsertarTrabajador />} />
-            <Route path="/gestTrabajador/modificarTrabajador/:id" element={<ModificarTrabajador />} />
+            <Route element={<RequireAuth allowedRoles={["SuperAdmin", "Admin", "user_User", "user_Supervisor", "user_Supervisado"]} />}>
+              <Route path="/monto_extra" element={<MontosExtra />} />
+            </Route>
             
-            {/* Agencia */}
-            <Route path="/gestAgencia/listarAgencias" element={<ListarAgencias />} />
-            <Route path="/gestAgencia/insertarAgencia" element={<InsertarAgencia />} />
+            <Route element={<RequireAuth allowedRoles={["Admin", "user_User", "user_Supervisor", "user_Supervisado"]} />}>
+              <Route path="/gestCobro/listarCobros" element={<ListarCobros />} />
+              <Route path="/gestCobro/insertarCobro" element={<InsertarCobro />} />
+              <Route path="/gestCobro/modificarCobro/:id" element={<ModificarCobro />} />
+            </Route>
 
-            {/* Cliente */}
-            <Route path="/gestCliente/listarClientes" element={<ListarClientes />} />
-            <Route path="/gestCliente/insertarCliente" element={<InsertarCliente />} />
-            <Route path="/gestCliente/modificarCliente/:id" element={<ModificarCliente />} />
+            <Route element={<RequireAuth allowedRoles={["SuperAdmin"]} />}>
+              {/* Rol */}
+              <Route path="/gestRol/listaUsuarios" element={<ListaUsuarios />} />
+              <Route path="/modificarRol/:id" element={<ModificarRol />} />
+              {/* PagoCliente */}
+              <Route path="/superVisarPagoCliente/listarPagosClientes" element={<ListarPagosClientes />} />
+              <Route path="/superVisarPagoCliente/modificarPagoCliente/" element={<ModificarPagoCliente />} />
+            </Route>
 
-            {/* Documentación */}
-            <Route path="/gestDocumentacion/listarDocumentacion" element={<ListarDocumentacion />} />
-            <Route path="/gestDocumentacion/insertarDocumentacion" element={<InsertarDocumentacion />} />
-            <Route path="/gestDocumentacion/modificarDocumentacion/:id" element={<ModificarDocumentacion />} />
+            <Route element={<RequireAuth allowedRoles={["SuperAdmin", "Admin"]} />}>
+              {/* Trabajador */}
+              <Route path="/gestTrabajador/listaTrabajadores" element={<ListaTrabajadores />} />
+              <Route path="/gestTrabajador/insertarTrabajador" element={<InsertarTrabajador />} />
+              <Route path="/gestTrabajador/modificarTrabajador/:id" element={<ModificarTrabajador />} />
+              
+              {/* Agencia */}
+              <Route path="/gestAgencia/listarAgencias" element={<ListarAgencias />} />
+              <Route path="/gestAgencia/insertarAgencia" element={<InsertarAgencia />} />
 
-            {/* Pago */}
-            <Route path="/gestPago/listarPagosCobros" element={<ListarPagosCobros />} />
-            <Route path="/gestPago/insertarPagoCobros" element={<InsertarPagoCobros />} />
-            <Route path="/gestPago/modificarPagoCobros/:id" element={<ModificarPagosCobros />} />
-            <Route path="/gestPago/listarPagosCobrosDetallados/:userId" element={<ListarPagosCobrosDetallados />} />
-            <Route path="/gestPago/listarRegistrosDeCobros/:cobroId" element={<ListarRegistrosDeCobros />} />
-            
-            {/*SuperVisarPagoCliente*/}
-            <Route path="/superVisarPagoCliente/comparativaTotalCobrar" element={<ComparativaTotalCobrar/>} />
+              {/* Cliente */}
+              <Route path="/gestCliente/listarClientes" element={<ListarClientes />} />
+              <Route path="/gestCliente/insertarCliente" element={<InsertarCliente />} />
+              <Route path="/gestCliente/modificarCliente/:id" element={<ModificarCliente />} />
 
+              {/* Documentación */}
+              <Route path="/gestDocumentacion/listarDocumentacion" element={<ListarDocumentacion />} />
+              <Route path="/gestDocumentacion/insertarDocumentacion" element={<InsertarDocumentacion />} />
+              <Route path="/gestDocumentacion/modificarDocumentacion/:id" element={<ModificarDocumentacion />} />
+
+              {/* Pago */}
+              <Route path="/gestPago/listarPagosCobros" element={<ListarPagosCobros />} />
+              <Route path="/gestPago/insertarPagoCobros" element={<InsertarPagoCobros />} />
+              <Route path="/gestPago/modificarPagoCobros/:id" element={<ModificarPagosCobros />} />
+              <Route path="/gestPago/listarPagosCobrosDetallados/:userId" element={<ListarPagosCobrosDetallados />} />
+              <Route path="/gestPago/listarRegistrosDeCobros/:cobroId" element={<ListarRegistrosDeCobros />} />
+              
+              {/*SuperVisarPagoCliente*/}
+              <Route path="/superVisarPagoCliente/comparativaTotalCobrar" element={<ComparativaTotalCobrar/>} />
+            </Route>
+
+            {/* Página de acceso denegado */}
+            <Route path="/unauthorized" element={<Unauthorized />} />
+
+            {/* Catch all: redireccionar a /home si la ruta no existe */}
+            <Route path="*" element={<Navigate to="/home" />} />
           </Route>
-
-          {/* Página de acceso denegado */}
-          <Route path="/unauthorized" element={<Unauthorized />} />
-
-          {/* Catch all: redireccionar a /home si la ruta no existe */}
-          <Route path="*" element={<Navigate to="/home" />} />
-        </Route>
-
-      </Routes>
+        </Routes>
+      </InactivityDetector>
     </Router>
   );
 }
